c12df08181c7b8c1deb2a61fbf3b1b7129e3e3b3
Joshua Vogelstein <joshuav@jhu.edu>
2014-03-16 16:49:22 -0400

    fixed plotting k

--- helper_fcns/set_benchmarks.m
+++ helper_fcns/set_benchmarks.m
@@ -1,11 +1,11 @@
 % profile on
 clearvars, clc, updatepath
 
-task_list_name='toeplitzs';
+task_list_name='Mai13';
 switch task_list_name
     case 'Mai13'
         task.ks=unique(round(logspace(0,2,50)));
-        task.Ntrials=100;
+        task.Ntrials=500;
         task.algs={'naivebayes','PDA','SLOL','LOL','DRDA','RDA','treebagger','svm'};
         task.savestuff=1;
     case 'thin'
