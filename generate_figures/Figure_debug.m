008b11851661a01226cc71841c3a21b88e7b0d87
Joshua Vogelstein <joshuav@jhu.edu>
2014-03-20 18:32:09 -0400

    making svd faster

--- generate_figures/Figure_debug.m
+++ generate_figures/Figure_debug.m
@@ -1,16 +1,20 @@
+profile on
 clearvars, clc,
 run([pwd,'/../helper_fcns/updatepath.m'])
 
-task.ntrain=10000;
-task.D=500;
+task.ntrain=1000;
+task.D=100;
 task.name='s';
-task.algs={'LDA','lda'};
+task.ks=100;
+task.algs={'LOL','PDA','LDA'};
 task.Ntrials=5;
 
 % task_list_name=task_list_names{i};
 % [T,P,S] = run_task_list(task_list_name);
 
 [T,P,S] = run_task(task);
+
+profile viewer
 
 F.plot_bayes=true;
 plot_benchmarks(task.name,F)
