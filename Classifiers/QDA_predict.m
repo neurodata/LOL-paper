0c9343135636d019d7156abec8f9954bb137fc85
Joshua Vogelstein <jovo@Joshuas-MacBook-Air.local>
2014-09-17 13:13:09 -0400

    added LDA and QDA predict

---
+++ Classifiers/QDA_predict.m
@@ -0,0 +1,27 @@
+function [Yhat,eta] = QDA_predict(X,P)
+% make predictions using LDA
+%
+% INPUT:
+%   X in R^{n x d}: data matrix where columns are data points
+%   P: structure containing all necessary parameters, including
+%       InvSig in R^{d x d}: inverse covariance matrix
+%       if Ngroups>2
+%           mu in R^{d x k}: mean of each of k groups
+%           c in R^k: constant for each
+%       if Ngroups=2
+%           del in R^{d x 1}: mu0-mu1
+%           thresh: (lnpi0-lnpi1)/2 in R_+: classification threshold
+%           mu  in R^d: overall mean
+
+% Outuput:
+%   Yhat in {0,1}^n: vector of predictions
+%   eta in R^{n x 1}: vector of magnitudes
+
+[~,ntest]=size(X);
+l=nan(ntest,P.Ngroups);
+for k=1:P.Ngroups
+    l(:,k)=-2*X'*P.InvSig(:,:,k)*P.mu(:,k)+P.c(k);
+end
+[~, ymax] = min(l,[],2);
+Yhat=P.groups(ymax);
+eta=[];
