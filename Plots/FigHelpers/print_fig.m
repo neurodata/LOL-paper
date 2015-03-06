78b5f8a94a4a9d79ec284e576ffa49ad528c6b9b
jovo <jovo@jhu.edu>
2015-03-06 13:33:23 -0500

    added option to print tiff

--- Plots/FigHelpers/print_fig.m
+++ Plots/FigHelpers/print_fig.m
@@ -10,10 +10,12 @@ end
 if ~isfield(F,'PaperSize'), F.PaperSize=[2 2]; end
 if ~isfield(F,'PaperPosition'), F.PaperPosition=[0 0 F.PaperSize]; end
 if ~isfield(F,'fname'), F.fname='temp_fig'; end
+if ~isfield(F,'tiff'), F.tiff=false; end
 set(h,'PaperSize',F.PaperSize,'PaperPosition',F.PaperPosition,'color','w');
 set(h, 'InvertHardCopy', 'off');
 set(h,'renderer',F.renderer)
 print(h,F.fname,'-dpdf')
 print(h,F.fname,'-dpng','-r300')
+if F.tiff, print(h,F.fname,'-dtiff','-r300'), end
 saveas(h,F.fname,'fig')
 
