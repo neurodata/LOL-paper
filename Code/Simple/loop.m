3af244c8f6e8956deb8d305cde786b236946695f
jovo <jovo@jhu.edu>
2016-12-27 11:10:04 -0500

    updated loop for good run

--- Code/Simple/loop.m
+++ Code/Simple/loop.m
@@ -21,25 +21,22 @@ algs={'LOL';'RRLDA';'QOQ';'LRL';'eigenfaces';'ROAD';'lasso'};
 
 A=length(algs);
 S=length(settings);
-nmc=2;
+nmc=40;
 
-% save('goodstuff.mat')
 
 %%
 
-% ks=nan(S,nmc);
-% D=nan(S,nmc);
-% ntrain=nan(S,nmc);
-% ntest=nan(S,nmc);
-for s=2
+for s=1:S
     setting=settings{s}
+    D=nan(nmc,1);
+    ntrain=nan(nmc,1);
+    ntest=nan(nmc,1);
     for i=1:nmc
         [Lhat(i),ks{i},D(i),ntrain(i),ntest(i)]=simple(setting,algs);
         fprintf('\n trial # %d\n', i)
     end
     save([setting, '.mat'],'Lhat','ks','D','ntrain','ntrain','algs','setting')
-    clear Lhat ks D ntrin ntest 
-    %     save('goodstuff.mat','Lhat','-append')
+    clear Lhat ks
 end
 
 
