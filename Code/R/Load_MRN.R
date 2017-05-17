5cf81839e6e02a2b6f44aa17b27a913c74aee0bf
Da Zheng <zhengda1936@gmail.com>
2017-05-17 00:05:31 -0400

    Load MRN data from S3.

--- Code/R/Load_MRN.R
+++ Code/R/Load_MRN.R
@@ -1,23 +1,30 @@
 library(FlashR)
 library(MASS)
 fm.set.conf("/mnt/nfs2/zhengda/FlashX/matrix/conf/run_test-EM.txt")
-df <- read.csv("/home/zhengda/ndmg.csv", header=TRUE)
-files <- list.files("/mnt/nfs/MR/data/MRN114/ndmg_v0011/reg_dti/bins/")
+df <- read.csv(url("http://neurodata-public-lol.s3-website-us-east-1.amazonaws.com/ndmg.csv"))
+files <- read.csv(url("http://neurodata-public-lol.s3-website-us-east-1.amazonaws.com/files.txt"),
+				  header=FALSE)
 vs <- list()
 num <- 0
 labels <- rep.int(0, length(df$Sex))
 for (idx in 1:length(df$URSI)) {
     id <- as.character(df$URSI[idx])
-    path <- paste("/mnt/nfs/MR/data/MRN114/ndmg_v0011/reg_dti/bins/MRN114_",
-        id, "_1_DTI_aligned.dat", sep="")
-    v <- fm.load.dense.matrix.bin(path, TRUE, 505472240, 1, FALSE, "F", id)
-    if (!is.null(v)) {
-        num <- num + 1
-        vs[[num]] <- v
-        labels[num] <- df$Sex[idx]
-    }
+	file <- paste("MRN114_", id, "_1_DTI_aligned.dat.gz", sep="")
+	path <- paste("http://neurodata-public-lol.s3-website-us-east-1.amazonaws.com/",
+				  file, sep="")
+	if (file %in% files$V1) {
+		print(path)
+		conn <- gzcon(url(path))
+		v <- fm.load.dense.matrix.bin(conn, TRUE, 505472240, 1, FALSE, "F", id)
+		if (!is.null(v)) {
+			num <- num + 1
+			vs[[num]] <- v
+			labels[num] <- df$Sex[idx]
+		}
+		close(conn)
+	}
 }
 data <- fm.cbind.list(vs)
-#tmp <- fm.conv.store(data, in.mem=FALSE, name="MRN.mat")
+tmp <- fm.conv.store(data, in.mem=FALSE, name="MRN-tmp.mat")
 save(labels, "/mnt/nfs2/zhengda/MRN.label")
 
