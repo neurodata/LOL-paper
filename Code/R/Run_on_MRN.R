d5775e6ef702ae308e489b306acbf9dc3ab36b78
Da Zheng <zhengda1936@gmail.com>
2017-05-30 15:46:34 -0400

    Update Run_on_MRN.R

--- Code/R/Run_on_MRN.R
+++ Code/R/Run_on_MRN.R
@@ -5,5 +5,5 @@ data <- fm.get.dense.matrix("MRN.mat")
 load("/mnt/nfs2/zhengda/MRN.label")
 source("run_test.R")
 
-rand.split.test(data, labels, 40, 0.9, c(1, seq(10, 100, 10)), TRUE)
+rand.split.test(data, labels, 40, 0.9, c(1, seq(10, 100, 10)))
 
