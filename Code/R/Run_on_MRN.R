464a0d24a854125ed1cfb96208708ba6db364833
Da Zheng <zhengda1936@gmail.com>
2017-05-30 10:23:13 -0400

    Update Run_on_MRN.R

--- Code/R/Run_on_MRN.R
+++ Code/R/Run_on_MRN.R
@@ -1,10 +1,8 @@
 library(FlashR)
 library(MASS)
-fm.set.conf("/mnt/nfs2/zhengda/FlashX/matrix/conf/run_test-EM.txt")
+fm.set.conf("/FlashX/matrix/conf/run_test.txt")
 data <- fm.get.dense.matrix("MRN.mat")
 load("/mnt/nfs2/zhengda/MRN.label")
-source("/mnt/nfs2/zhengda/FlashX-stable/Rpkg/R/SVD.R")
-source("/mnt/nfs2/zhengda/FlashX-stable/FlashR-learn/R/LOL.R")
 source("run_test.R")
 
 rand.split.test(data, labels, 40, 0.9, c(1, seq(10, 100, 10)), TRUE)
