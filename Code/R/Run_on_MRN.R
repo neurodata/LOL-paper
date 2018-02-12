d3c7cf506e61af7ce697b6da149e16c6712055e2
eric bridgeford <ebridge2@jhu.edu>
2018-02-12 04:15:31 -0500

    updated mrn running

--- Code/R/Run_on_MRN.R
+++ Code/R/Run_on_MRN.R
@@ -2,7 +2,7 @@ library(FlashR)
 library(MASS)
 fm.set.conf("/FlashX/matrix/conf/run_test.txt")
 data <- fm.get.dense.matrix("MRN.mat")
-load("/mnt/nfs2/zhengda/MRN.label")
+load("MRN.label")
 source("run_test.R")
 
 rand.split.test(data, labels, 40, 0.9, c(1, seq(10, 100, 10)))
